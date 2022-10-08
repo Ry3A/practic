ab0587ddd701046501f7267eb6752a0a9485ce11
apl26 <apl2612@mail.ru>
2022-10-08 16:43:03 +0300

    new, 6 lab

---
+++ src/lab6/Test.java
@@ -0,0 +1,9 @@
+package lab6;
+
+public class Test {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.selectionSort();
+        student.PrintScr();
+    }
+}
